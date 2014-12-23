d4c09447feb6675a0efdbd23fcee9b63982cabaf
LookNya <pavelx58@yandex.ru>
2014-12-23 03:48:43 +0300

    secs and records

--- scripts/main.js
+++ scripts/main.js
@@ -5,22 +5,34 @@ document.onreadystatechange = function () {
 }
 function init(){
 	start.addEventListener('touchstart',game.start)
-	reset.addEventListener('touchstart',game.reset)
+	resetTRUE.addEventListener('touchstart',game.reset)
 	for(var i = 0; i < document.getElementsByClassName('nbut').length; i++){
 		game.setNums(i)
 		document.getElementsByClassName('nbut')[i].addEventListener('touchstart', game.nbutClick)
 	}
+	game.initRecordsDiv()
+
 }
 
 game = {
+	stage: 0,
+	allStages: 1,
+	startSecs: 0,
+	secsViewInterval: '',
 	start: function(){
 		content.className = 'enter'
 		game.step = 0
+		clearInterval(game.secsViewInterval)
 		game.enter()
+		
 	},
 	reset: function(){
 		content.className = 'begining'
+		secs.innerHTML = ''
 		game.step = 0
+		clearTimeout(game.enteringTimer)
+		clearInterval(game.secsViewInterval)
+		game.initRecordsDiv()
 	},
 	nbutClick: function(e){
 		var num = (e.target.dataset.num)%10
@@ -38,14 +50,20 @@ game = {
 			document.getElementsByClassName('nbut')[i].dataset.num = i+1
 		}
 	},
+	enteringTimer:'',
+	st2Timer:'',
+	st3Timer:'',
 	enter: function(){
 		var first = utils.rnd10(), second = utils.rnd10()
 		game.prev[1] = first*second
 		task.innerHTML = '<div>'+first+' * '+second+'</div>'
 		gbcount.className = 'st1'
-		setTimeout(function(){gbcount.className = 'st2'}, 1000)
-		setTimeout(function(){gbcount.className = 'st3'}, 2000)
-		setTimeout(function(){content.className = 'playing'; game.newStep();gbcount.className = ''}, 3000)
+		clearTimeout(game.enteringTimer)
+		clearTimeout(game.st2Timer)
+		clearTimeout(game.st3Timer)
+		game.st2Timer = setTimeout(function(){gbcount.className = 'st2'}, 1000)
+		game.st3Timer = setTimeout(function(){gbcount.className = 'st3'}, 2000)
+		game.enteringTimer = setTimeout(function(){content.className = 'playing'; game.newStep();gbcount.className = ''}, 3000)
 	},
 	step: 0,
 	prev: [0,0],
@@ -59,10 +77,53 @@ game = {
 		game.prev[game.prev.length-1] = first*second
 		task.innerHTML = '<div>'+first+' * '+second+'</div>'
 		game.step++
-		if(game.step == 9){
-			alert("That's all.")
-			game.reset()
+		if(game.step == 3){
+			game.end()
 		}
+		if(game.step == 1){
+			game.secsViewInterval = setInterval(game.refreshSecs,100)
+			game.startSecs = new Date()
+		}
+	},
+	getRecords: function(stage){
+		if(localStorage['myData4']){
+			var buff = JSON.parse(localStorage['myData4']).data
+			for(var i = 0; i<game.allStages; i++){
+				if(buff[i][0]==stage){
+					return buff[i][1]
+				} else {
+					return 0
+				}
+			}
+		}
+	},
+	saveRecord: function(stage, record){
+		if(localStorage['myData4']){
+			var buff = JSON.parse(localStorage['myData4']).data
+			for(var i = 0; i<game.allStages; i++){
+				if(buff[i][0]==stage){
+					buff[i][1] = record
+				} else {
+				buff.push([[stage],[record]])
+				}
+			}
+			
+			localStorage['myData4']=JSON.stringify({"data":buff})
+			return;
+		}	
+		var buff =[[stage],[record]]
+		localStorage['myData4']=JSON.stringify({"data":[buff]})
+	},
+	end: function(){
+		clearInterval(game.secsViewInterval)
+		var resTime = new Date() - game.startSecs
+		var msg = 'Стадия пройдена за '+utils.normalTime(resTime)+'. \n'
+		if(resTime < game.getRecords(game.stage)){
+			msg+='Новый рекорд!'
+			game.saveRecord(game.stage, resTime)
+		}
+		alert(msg)
+		game.reset()
 	},
 	respColorTimeout: '',
 	respCorr: function(correct){
@@ -74,11 +135,25 @@ game = {
 		} else {
 			controls.className = 'err'
 		}
-		game.respColorTimeout = setTimeout(function(){controls.className = ''}, 300)
+		game.respColorTimeout = setTimeout(function(){controls.className = ''}, 150)
 	},
+	refreshSecs: function(){
+		secs.innerHTML =  utils.normalTime(new Date() - game.startSecs)	
+	},
+	initRecordsDiv: function(){
+		var str = ''
+		for(var i = 0; i<game.allStages; i++){
+			var lol = i+1
+			str += 'Рекорд стадии '+lol+' — '+utils.normalTime(game.getRecords(i)) + '<br>'
+		}	
+		records.innerHTML = str
+	}
 }
 utils = {
 	rnd10: function(){
 		return Math.round(Math.random() * (9 - 1) + 1)
+	},
+	normalTime: function(time){
+		return time/1000 + ' секунд'
 	}
 }
